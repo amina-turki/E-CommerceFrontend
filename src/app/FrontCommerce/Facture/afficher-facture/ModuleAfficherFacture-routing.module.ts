1e11225ebd76894621157ae3e569ef57f1abed78
Chaima_27 <zaibichaima50@gmail.com>
2022-04-01 07:51:21 +0100

    routes

--- src/app/FrontCommerce/Facture/afficher-facture/ModuleAfficherFacture-routing.module.ts
+++ src/app/FrontCommerce/Facture/afficher-facture/ModuleAfficherFacture-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AfficherProduitComponent } from '../../Produit/afficher-produit/afficher-produit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AfficherProduitComponent,
+    data: {
+      breadcrumb: 'Factures',
+      icon: 'icofont-table bg-c-blue',
+      //breadcrumb_caption: 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit - Basic Table',
+      status: true
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/FrontCommerce/Facture/afficher-facture/ModuleAfficherFacture.module.ts
+++ src/app/FrontCommerce/Facture/afficher-facture/ModuleAfficherFacture.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModuleAfficherFactureRoutingModule } from './ModuleAfficherFacture-routing.module';
+import { AfficherFactureComponent } from './afficher-facture.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AfficherFactureComponent],
   imports: [
     CommonModule,
     ModuleAfficherFactureRoutingModule
